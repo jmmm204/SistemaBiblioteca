70c184363c11bb3ab90f3d10acdf4d89dffcbac6
jmmm204 <164056385+jmmm204@users.noreply.github.com>
2025-10-16 22:48:53 -0600

    Tercer avance del sistema. Realizado por Arnuero

---
+++ SistemaBiblioteca2/src/main/java/entities/Libro.java
@@ -0,0 +1,42 @@
+package entities;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.HashSet;
+import java.util.Set;
+
+@Entity
+@Getter @Setter
+public class Libro {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    
+    private String titulo;
+
+    private int anioPublicacion;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "autor_id")
+    private Autor autor;
+
+    @ManyToMany(cascade = { CascadeType.PERSIST, CascadeType.MERGE })
+    @JoinTable(
+            name = "libro_categoria",
+            joinColumns = @JoinColumn(name = "libro_id"),
+            inverseJoinColumns = @JoinColumn(name = "categoria_id")
+    )
+    private Set<Categoria> categorias = new HashSet<>();
+
+    public Libro() {
+    }
+
+    public Libro(String titulo, int anioPublicacion, Autor autor) {
+        this.titulo = titulo;
+        this.anioPublicacion = anioPublicacion;
+        this.autor = autor;
+    }
+}
